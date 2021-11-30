30587915349231e3e700a6f3b5c988dbcbd20fcf
makersmelx <makersmelx@gmail.com>
2021-11-30 15:36:25 -0500

    feat(*): navigation and tab demo code

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 import BottomTabs, { BottomTabView } from '../../component/BottomTabs';
+import { AuthService } from '../../service';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,22 +15,45 @@ const styles = StyleSheet.create({
   },
 });
 
-const Index: React.FC = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <BottomTabs>
-      <BottomTabView title="test1">
-        <Text>heyhey123</Text>
-      </BottomTabView>
-      <BottomTabView title="test2">
-        <Text>heyhey234</Text>
-      </BottomTabView>
-      <BottomTabView title="test3">
-        <Text>heyhey2345</Text>
-      </BottomTabView>
-    </BottomTabs>
-    <StatusBar />
-  </View>
-);
+const Index: React.FC = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Test Test',
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <BottomTabs>
+        <BottomTabView title="My Wish">
+          <Text>My Wish</Text>
+          <Button
+            onPress={async () => {
+              await AuthService.signUp('makersmelx@gmail.com', '123456');
+            }}
+            title="Sign Up"
+          />
+        </BottomTabView>
+        <BottomTabView title="Claimed">
+          <Text>heyhey</Text>
+          <Button
+            onPress={async () => {
+              await AuthService.signIn('makersmelx@gmail.com', '123456');
+            }}
+            title="Login"
+          />
+        </BottomTabView>
+        <BottomTabView title="Friends">
+          <Text>heyhey234</Text>
+        </BottomTabView>
+        <BottomTabView title="Profile">
+          <Text>heyhey2345</Text>
+        </BottomTabView>
+      </BottomTabs>
+      <StatusBar />
+    </View>
+  );
+};
 
 export default Index;
