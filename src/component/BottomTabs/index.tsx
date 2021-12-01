fc3d02fa4ee40afd9288fa1d4a072c7efb63f297
makersmelx <makersmelx@gmail.com>
2021-11-30 22:49:07 -0500

    feat(#12): switch to native base

--- src/component/BottomTabs/index.tsx
+++ src/component/BottomTabs/index.tsx
@@ -1,17 +1,12 @@
 import React, { ReactElement, useState } from 'react';
-import { Tab, TabView } from 'react-native-elements';
 import { StyleProp, ViewStyle } from 'react-native';
 
+import { Box, Center, HStack, Pressable, Text } from 'native-base';
+
 interface IBottomTabView {
-  // eslint-disable-next-line react/no-unused-prop-types
-  title:
-    | string
-    | React.ReactElement<
-        {},
-        // eslint-disable-next-line no-unused-vars
-        string | ((props: any) => React.ReactElement<any, any>) | { new (props: any): React.Component<any, any, any> }
-      >
-    | undefined;
+  title: any;
+  icon?: any;
+  iconSelected?: any;
 }
 
 interface IBottomTab {
@@ -22,21 +17,32 @@ interface IBottomTab {
 const BottomTabView: React.FC<IBottomTabView> = ({ children }) => <>{children}</>;
 
 const Index: React.FC<IBottomTab> = ({ children, tabStyle, viewStyle }) => {
-  const [active, setActive] = useState(0);
-  const tabViews = React.Children.map(children, (child) => <TabView.Item style={viewStyle}>{child}</TabView.Item>);
-  const tabs = React.Children.map(children, (child) => {
+  const [selected, setSelected] = useState(1);
+  const tabViews = <Center style={viewStyle}>{React.Children.toArray(children)[selected]}</Center>;
+  const tabs = React.Children.map(children, (child, index) => {
     const c = child as ReactElement<IBottomTabView>;
-    return <Tab.Item style={tabStyle} title={c.props.title} />;
+    return (
+      <Pressable opacity={selected === index ? 1 : 0.5} flex={1} onPress={() => setSelected(index)} style={tabStyle}>
+        <Center>
+          {selected === index ? c.props.iconSelected : c.props.icon}
+          {typeof c.props.title === 'string' ? (
+            <Text color="white" fontSize="12">
+              {c.props.title}
+            </Text>
+          ) : (
+            c.props.title
+          )}
+        </Center>
+      </Pressable>
+    );
   });
   return (
-    <>
-      <TabView value={active - 2} onChange={setActive}>
-        {tabViews}
-      </TabView>
-      <Tab value={active} onChange={setActive}>
+    <Box flex={1} bg="white" safeAreaTop>
+      <Center flex={1}>{tabViews}</Center>
+      <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
         {tabs}
-      </Tab>
-    </>
+      </HStack>
+    </Box>
   );
 };
 
