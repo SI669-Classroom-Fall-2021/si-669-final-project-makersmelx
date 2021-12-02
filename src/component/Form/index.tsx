5ddd894cd89c1ade13854423a3b5e5f65934eabe
makersmelx <makersmelx@gmail.com>
2021-12-01 21:25:04 -0500

    feat（#14): set the field of form component to optional

--- src/component/Form/index.tsx
+++ src/component/Form/index.tsx
@@ -6,20 +6,21 @@ import FormItem from './FormItem';
 interface IForm {
   space: number;
   onFinish: (value: any) => void;
-  onError: (error: any) => void;
-  submitButton: string | ReactElement;
+  onError?: (error: any) => void;
+  submitButton?: string | ReactElement;
 }
 
 const Index: React.FC<IForm> = ({ children, onFinish, onError, space, submitButton }) => {
   const formMethods = useForm();
   const button =
-    typeof submitButton === 'string' ? (
+    submitButton &&
+    (typeof submitButton === 'string' ? (
       <Button onPress={formMethods.handleSubmit(onFinish, onError)}>{submitButton}</Button>
     ) : (
       cloneElement(submitButton, {
         onPress: formMethods.handleSubmit(onFinish, onError),
       })
-    );
+    ));
   return (
     <FormProvider {...formMethods}>
       <Column space={space}>
