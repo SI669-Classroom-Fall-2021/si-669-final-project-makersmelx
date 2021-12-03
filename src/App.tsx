e12b53136a03bbd16cfd54d091bd3885372ba564
makersmelx <makersmelx@gmail.com>
2021-12-03 09:47:48 -0500

    feat(#25): auth route with onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
-import Home from './page/Home';
-import SignUp from './page/SignUp';
-import SignIn from './page/SignIn';
-import { AuthService } from './service';
+import Route from './Route';
 
-const Stack = createNativeStackNavigator();
-
-const Index: React.FC = () => {
-  useEffect(() => {
-    AuthService.subscribeAuth();
-  }, []);
-  return (
+const Index: React.FC = () => (
     <NavigationContainer>
       <NativeBaseProvider>
-        <Stack.Navigator initialRouteName={AuthService.auth.currentUser ? 'Home' : 'SignIn'}>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="SignIn" component={SignIn} />
-        </Stack.Navigator>
+        <Route />
       </NativeBaseProvider>
     </NavigationContainer>
   );
-};
 
 registerRootComponent(Index);
