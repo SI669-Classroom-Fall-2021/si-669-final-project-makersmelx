d18a21255a6868c6e046e11190b4f32e8c3e6f72
makersmelx <makersmelx@gmail.com>
2021-12-02 21:17:56 -0500

    feat(#25): sign in page with fail message

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
 import Home from './page/Home';
 import SignUp from './page/SignUp';
+import SignIn from './page/SignIn';
 import { AuthService } from './service';
 
 const Stack = createNativeStackNavigator();
@@ -16,9 +17,10 @@ const Index: React.FC = () => {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName={AuthService.auth.currentUser ? 'Home' : 'SignIn'}>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignIn" component={SignIn} />
         </Stack.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
