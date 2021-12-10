15e89515f789e77eb6402cb0ecc663d4a46ee847
makersmelx <makersmelx@gmail.com>
2021-12-10 02:06:33 -0500

    feat(*): the detail friend wish page and warning modification

--- src/page/Friends/FriendWish.tsx
+++ src/page/Friends/FriendWish.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import {
   Box,
   Button,
@@ -7,13 +7,14 @@ import {
   HStack,
   Image,
   Link,
-  Text,
-  VStack,
   ScrollView,
-  useToast
+  Text,
+  useToast,
+  VStack
 } from 'native-base';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons
+  from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useRequest } from 'ahooks';
 import { AuthService, ClaimService, IWish, WishService } from '../../service';
 
@@ -23,17 +24,22 @@ const Index: React.FC = () => {
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const { content, friendID }: { content: IWish; friendID: string } =
-    route.params;
+  const {
+    content,
+    friendID
+  }: { content: IWish; friendID: string } = route.params;
   const toast = useToast();
   const claimFailToast = 'claim-fail-toast';
+  useLayoutEffect(() => {
+    // todo: should be the friend name
+    navigation.setOptions({
+      title: content.name,
+    });
+  });
   const { run, loading } = useRequest(
     async () => {
-      ClaimService.claimWish(
-        AuthService.auth.currentUser?.uid || '',
-        friendID,
-        content.key,
-      );
+      await ClaimService.claimWish(
+        AuthService.auth.currentUser?.uid || '', friendID, content.key);
     },
     {
       manual: true,
@@ -53,70 +59,76 @@ const Index: React.FC = () => {
   );
   return (
     <Center flex={1}>
-      <Box style={{ height: '100%', width: '90%' }}>
-        <VStack style={{ height: '100%', width: '100%' }}>
-          <HStack flex={0.5} justifyContent="space-between" alignItems="center">
+      <Center style={{ height: '100%', width: '100%' }} bg="white">
+        <VStack style={{ height: '100%', width: '90%' }}>
+          <HStack
+            flex={0.5}
+            justifyContent="space-between"
+            alignItems="center"
+            mt={6}
+          >
             <Heading size="lg" textAlign="left" padding={3}>
               {content.name}
             </Heading>
             <Text fontSize="lg" padding={3}>{`$${content.price}`}</Text>
           </HStack>
+
           <Center flex={3} marginBottom={3} h="30%">
             <Image
               size="100%"
               resizeMode="cover"
-              borderRadius={10}
+              mt={6}
               source={{
                 uri: content.image,
               }}
               alt="gift"
-              fallbackElement={
-                <MaterialCommunityIcons
-                  name="gift-outline"
-                  color="black"
-                  size={30}
-                />
-              }
+              borderRadius={6}
+              fallbackElement={<MaterialCommunityIcons
+                name="gift-outline"
+                color="black"
+                size={30}
+              />}
             />
           </Center>
           <Box flex={3}>
-            <Box
-              h="65%"
-              bg="primary.100"
-              marginY="3"
-              borderRadius={10}
-              paddingX={5}
-              paddingY={3}
-            >
-              <Heading size="md" textAlign="left" marginY="1">
-                Description
-              </Heading>
+            <Box bg="white" paddingX={5} paddingY={3} mt={2}>
               <ScrollView>
                 <Text>{content.description}</Text>
               </ScrollView>
             </Box>
             <Box
-              bg="primary.100"
-              marginY="3"
-              borderRadius={10}
+              bg="white"
               paddingX={5}
               paddingY={3}
+              borderRadius={6}
+              borderColor="gray.200"
+              borderWidth={1}
             >
-              <Link href={content.url} isExternal>
+              <Link
+                href={content.url}
+                isExternal
+                _text={{
+                  _light: {
+                    color: 'cyan.500',
+                  },
+                  color: 'cyan.300',
+                }}
+                isUnderlined
+              >
                 {content.url}
               </Link>
             </Box>
+            <Button
+              isLoading={loading}
+              isDisabled={content.state !== WishService.WishState.Default}
+              onPress={run}
+              mt={10}
+            >
+              Claim for my friend
+            </Button>
           </Box>
-          <Button
-            isLoading={loading}
-            marginBottom={3}
-            isDisabled={content.state !== WishService.WishState.Default}
-            onPress={run}
-          >
-            Claim for him
-          </Button>
         </VStack>
-      </Box>
+      </Center>
     </Center>
   );
 };
