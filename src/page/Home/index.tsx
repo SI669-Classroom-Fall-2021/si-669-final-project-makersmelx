128a475b519169417f2acb48c4bd2049e227c4f2
makersmelx <makersmelx@gmail.com>
2021-12-16 19:08:38 -0500

    feat(#70): tab bar

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -4,7 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons
   from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useTheme } from 'native-base';
+import { Column, Text, useTheme } from 'native-base';
 import WishListStack from '../WishList';
 import Friends from '../Friends';
 import Claimed from '../Claimed';
@@ -20,65 +20,67 @@ const Index: React.FC = () => {
     });
   }, [navigation]);
   const { colors } = useTheme();
+
+  const NaviIcon = (props: any, title: string, iconName: string) => (
+    <Column
+      width={50}
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+    >
+      <MaterialCommunityIcons
+        name={iconName}
+        color={props.color}
+        size={24}
+      />
+      <Text color={props.color} fontSize={10}>{title}</Text>
+    </Column>
+  )
+
   return (
     <Tab.Navigator
       activeColor={(colors.primary as any)['500']}
       barStyle={{ backgroundColor: 'white' }}
+      labeled={false}
     >
       <Tab.Screen
         name="WishListStack"
         component={WishListStack}
         options={{
-          tabBarLabel: 'Wish List',
-          tabBarIcon: (props: any) => <MaterialCommunityIcons
-            name={props.focused ? 'heart' : 'heart-outline'}
-            color={props.color}
-            size={26}
-          />
+          tabBarIcon: (iconProps: any) => NaviIcon(
+            iconProps, 'Wish List',
+            iconProps.focused ? 'heart' : 'heart-outline'
+          )
         }}
       />
       <Tab.Screen
         name="Claimed"
         component={Claimed}
         options={{
-          tabBarLabel: 'Claimed',
-          tabBarIcon: (props: any) => (
-            <MaterialCommunityIcons
-              name={props.focused ? 'bookmark-check' : 'bookmark-check-outline'}
-              color={props.color}
-              size={26}
-            />
-          ),
+          tabBarIcon: (iconProps: any) =>
+            NaviIcon(iconProps, 'Claimed', iconProps.focused
+              ? 'bookmark-check'
+              : 'bookmark-check-outline')
         }}
       />
       <Tab.Screen
         name="Friends"
         component={Friends}
         options={{
-          tabBarLabel: 'Friends',
-          tabBarIcon: (props: any) => (
-            <MaterialCommunityIcons
-              name={props.focused
-                ? 'account-multiple'
-                : 'account-multiple-outline'}
-              color={props.color}
-              size={26}
-            />
-          ),
+          tabBarIcon: (iconProps: any) =>
+            NaviIcon(iconProps, 'Friends', iconProps.focused
+              ? 'account-multiple'
+              : 'account-multiple-outline')
         }}
       />
       <Tab.Screen
         name="Settings"
         component={Settings}
         options={{
-          tabBarLabel: 'Settings',
-          tabBarIcon: (props: any) => (
-            <MaterialCommunityIcons
-              name={props.focused ? 'cog' : 'cog-outline'}
-              color={props.color}
-              size={26}
-            />
-          ),
+          tabBarIcon: (iconProps: any) =>
+            NaviIcon(iconProps, 'Settings', iconProps.focused
+              ? 'cog'
+              : 'cog-outline')
         }}
       />
     </Tab.Navigator>
